025d1449701d9110d2b179a6ee91476290a004c8
TSUDAKUN <shunta0919@icloud.com>
2020-05-14 15:46:15 +0900

    テトリス作る準備

--- TetodannProject/tetris.cpp
+++ TetodannProject/tetris.cpp
@@ -3,16 +3,34 @@
 #include "keycheck.h"
 #include "tetris.h"
 
+int minoImage[MINO_MAX];					// ﾐﾉ画像
+
 int mapData[DATA_MAX_Y][DATA_MAX_X];		// ﾃﾞｰﾀ格納用
+int typeBlock;								// ﾌﾞﾛｯｸの種類格納用
+
+int moveBlock[4][4];
+
+BLOCK_TYPE next[BLOCK_TYPE_MAX];
+BLOCK_TYPE next2[BLOCK_TYPE_MAX];
+
+BLOCK blockType[BLOCK_TYPE_MAX];
 
 
+
+// ﾃﾄﾘｽ関連ｼｽﾃﾑ系初期化
 bool TetrisSysInit(void)
 {
 	bool rtnFlag = true;
 
+	MinoInit();
+
+	LoadDivGraph("Image/mino.png", MINO_MAX, MINO_MAX, 1, MINO_SIZE_X, MINO_SIZE_Y, &minoImage[0]);
+
 	return rtnFlag;
 }
 
+
+// ﾃﾄﾘｽ関連初期化
 void TetrisInit(void)
 {
 	for (int x = 0; x < DATA_MAX_X; x++)
@@ -22,24 +40,230 @@ void TetrisInit(void)
 			mapData[y][x] = -1;
 			if (y == 0 || y == DATA_MAX_Y - 1)
 			{
-				mapData[y][x] = 1;
+				mapData[y][x] = 7;
 			}
 			if (x == 0 || x == DATA_MAX_X - 1)
 			{
-				mapData[y][x] = 1;
+				mapData[y][x] = 7;
 			}
 		}
 	}
 }
 
+
+// ﾐﾉ情報初期化
+void MinoInit(void)
+{
+	// ﾃﾞｰﾀ格納
+	for (int a = 0; a < BLOCK_TYPE_MAX; a++)
+	{
+		for (int b = 0; b < DIR_MAX; b++)
+		{
+			for (int c = 0; c < 4; c++)
+			{
+				for (int d = 0; d < 4; d++)
+				{
+					blockType[a].block[b][c][d] = -1;
+				}
+			}
+		}
+	}
+
+	// BROCK_TIPE_I
+	blockType[BLOCK_TYPE_I].block[DIR_0][1][0] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_0][1][2] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_0][1][3] = 1;
+
+	blockType[BLOCK_TYPE_I].block[DIR_1][0][1] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_1][1][1] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_1][2][1] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_1][3][1] = 1;
+
+	blockType[BLOCK_TYPE_I].block[DIR_2][2][0] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_2][2][1] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_2][2][2] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_2][2][3] = 1;
+
+	blockType[BLOCK_TYPE_I].block[DIR_3][0][2] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_3][2][2] = 1;
+	blockType[BLOCK_TYPE_I].block[DIR_3][3][2] = 1;
+
+
+
+	// BLOCK_TYPE_J
+	blockType[BLOCK_TYPE_J].block[DIR_0][0][0] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_0][1][0] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_0][1][2] = 1;
+
+	blockType[BLOCK_TYPE_J].block[DIR_1][0][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_1][0][2] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_1][1][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_1][2][1] = 1;
+
+	blockType[BLOCK_TYPE_J].block[DIR_2][1][0] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_2][1][2] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_2][2][2] = 1;
+
+	blockType[BLOCK_TYPE_J].block[DIR_3][0][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_3][1][1] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_3][2][0] = 1;
+	blockType[BLOCK_TYPE_J].block[DIR_3][2][1] = 1;
+
+
+
+	// BLOCK_TYPE_L
+	blockType[BLOCK_TYPE_L].block[DIR_0][0][3] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_0][1][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_0][1][3] = 1;
+
+	blockType[BLOCK_TYPE_L].block[DIR_1][0][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_1][1][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_1][2][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_1][2][3] = 1;
+
+	blockType[BLOCK_TYPE_L].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_2][1][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_2][1][3] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_2][2][1] = 1;
+
+	blockType[BLOCK_TYPE_L].block[DIR_3][0][1] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_3][0][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_L].block[DIR_3][2][2] = 1;
+
+
+
+	// BLOCK_TYPE_S
+	blockType[BLOCK_TYPE_S].block[DIR_0][1][2] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_0][1][3] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_0][2][1] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_0][2][2] = 1;
+
+	blockType[BLOCK_TYPE_S].block[DIR_1][0][1] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_1][1][1] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_1][1][2] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_1][2][2] = 1;
+
+	blockType[BLOCK_TYPE_S].block[DIR_2][0][2] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_2][0][3] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_2][1][2] = 1;
+
+	blockType[BLOCK_TYPE_S].block[DIR_3][0][2] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_3][1][3] = 1;
+	blockType[BLOCK_TYPE_S].block[DIR_3][2][3] = 1;
+
+
+
+	// BLOCK_TYPE_Z
+	blockType[BLOCK_TYPE_Z].block[DIR_0][1][0] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_0][2][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_0][2][2] = 1;
+
+	blockType[BLOCK_TYPE_Z].block[DIR_1][0][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_1][1][0] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_1][1][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_1][2][0] = 1;
+
+	blockType[BLOCK_TYPE_Z].block[DIR_2][0][0] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_2][0][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_2][1][2] = 1;
+
+	blockType[BLOCK_TYPE_Z].block[DIR_3][0][2] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_3][1][1] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_Z].block[DIR_3][2][1] = 1;
+
+
+
+	// BLOCK_TYPE_T
+	blockType[BLOCK_TYPE_T].block[DIR_0][0][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_0][1][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_0][1][3] = 1;
+
+	blockType[BLOCK_TYPE_T].block[DIR_1][0][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_1][1][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_1][1][3] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_1][2][2] = 1;
+
+	blockType[BLOCK_TYPE_T].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_2][1][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_2][1][3] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_2][2][2] = 1;
+
+	blockType[BLOCK_TYPE_T].block[DIR_3][0][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_3][1][1] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_T].block[DIR_3][2][2] = 1;
+
+
+
+	// BLOCK_TYPE_O
+	blockType[BLOCK_TYPE_O].block[DIR_0][1][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_0][1][2] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_0][2][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_0][2][2] = 1;
+
+	blockType[BLOCK_TYPE_O].block[DIR_1][1][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_1][1][2] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_1][2][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_1][2][2] = 1;
+
+	blockType[BLOCK_TYPE_O].block[DIR_2][1][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_2][1][2] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_2][2][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_2][2][2] = 1;
+
+	blockType[BLOCK_TYPE_O].block[DIR_3][1][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_3][1][2] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_3][2][1] = 1;
+	blockType[BLOCK_TYPE_O].block[DIR_3][2][2] = 1;
+}
+
+
+
 void TetrisCtl(void)
 {
+	// 動かすブロックがあるかどうかのチェック
+	// なければ出現処理
 
+	// next2が空であれば、補充の処理
+
+	// 座標のバックアップ
+
+	// キー情報の取得
+	// キー情報の移動処理、回転処理
+
+	// 自動の移動処理
+
+	// 当たり判定処理
+	// 当たっていればフラグを切り替える
+
+	// 座標の保存
 }
 
 void TetrisDraw(void)
 {
-
+	for (int y = 0; y < DATA_MAX_Y; y++)
+	{
+		for (int x = 0; x < DATA_MAX_X; x++)
+		{
+			if (mapData[y][x] != -1)
+			{
+				DrawGraph((x - 1) * MINO_SIZE_X + 16, (y - 4) * MINO_SIZE_Y + 16, minoImage[mapData[y][x]], true);
+			}
+		}
+	}
+	
 }
 
 int TetrisLine(void)
--- TetodannProject/tetris.h
+++ TetodannProject/tetris.h
@@ -3,8 +3,71 @@
 #define DATA_MAX_X 12
 #define DATA_MAX_Y 25
 
-bool TetrisSysInit(void);
-void TetrisInit(void);
+#define MINO_MAX 8      // ﾐﾉ種類
+#define MINO_SIZE_X 40
+#define MINO_SIZE_Y 40
+
+
+
+// ﾌﾞﾛｯｸの種類
+enum BLOCK_TYPE
+{
+	BLOCK_TYPE_I,
+	BLOCK_TYPE_J,
+	BLOCK_TYPE_L,
+	BLOCK_TYPE_S,
+	BLOCK_TYPE_Z,
+	BLOCK_TYPE_T,
+	BLOCK_TYPE_O,
+	BLOCK_TYPE_MAX
+};
+
+
+// ﾌﾞﾛｯｸの向き
+enum BLOCK_DIR
+{
+	DIR_0,
+	DIR_1,
+	DIR_2,
+	DIR_3,
+	DIR_MAX
+};
+
+
+// 移動の方向
+enum MOVE_DIR
+{
+	MOVE_DIR_DOWN,
+	MOVE_DIR_LEFT,
+	MOVE_DIR_RIGHT,
+	MOVE_DIR_MAX
+};
+
+
+// 座標
+struct Pos
+{
+	int x;
+	int y;
+};
+
+
+// ブロック
+struct BLOCK
+{
+	int block[DIR_MAX][4][4];
+	Pos pos;
+	bool flag;
+};
+
+
+
+
+
+
+bool TetrisSysInit(void);		// ﾃﾄﾘｽ関連ｼｽﾃﾑ系初期化
+void TetrisInit(void);			// ﾃﾄﾘｽ関連初期化
+void MinoInit(void);			// ﾐﾉ情報初期化
 void TetrisCtl(void);
 void TetrisDraw(void);
 
