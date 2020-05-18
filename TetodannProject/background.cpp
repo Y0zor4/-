4b7441f33250d4be1abdbeaa3328ba9e89dcf64c
TSUDAKUN <shunta0919@icloud.com>
2020-05-19 02:20:42 +0900

    背景表示

--- TetodannProject/background.cpp
+++ TetodannProject/background.cpp
@@ -3,11 +3,12 @@
 #include "keycheck.h"
 #include "background.h"
 
+int bgImageTmp;
 
 bool BackgroundSysInit(void)
 {
 	bool rtnFlag = true;
-
+	bgImageTmp = LoadGraph("image/bg_tmp.png");
 	return rtnFlag;
 }
 
@@ -23,5 +24,6 @@ void BackgroundCtl(int floor)
 
 void BackgroundDraw(int floor)
 {
-	DrawBox(OFFSET_X, OFFSET_Y, BG_SIZE_X + OFFSET_X, BG_SIZE_Y + OFFSET_Y, 0x00FFFF, true);
+	DrawBox(OFFSET_X, OFFSET_Y, BG_SIZE_X + OFFSET_X, BG_SIZE_Y + OFFSET_Y, 0x000000, true);
+	DrawGraph(OFFSET_X, OFFSET_Y, bgImageTmp, true);
 }
