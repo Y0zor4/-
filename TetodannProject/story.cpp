4c69929e4d223f244fa83ad75ea764836aa8fdaa
Y0zor4 <kirakirahikaru1333@icloud.com>
2020-05-20 12:58:01 +0900

    ソースコードの追加

--- TetodannProject/story.cpp
+++ TetodannProject/story.cpp
@@ -34,6 +34,19 @@ void StoryDraw(void)
 	ClsDrawScreen();
 	SetFontSize(50);
 	DrawFormatString(0, 0, 0xFFFFFF, "StoryScene");
-	DrawBox(100, 100, 700, 500, 0x00FF00, true);
+	//DrawBox(100, 100, 700, 500, 0x00FF00, true);
+	DrawFormatString(100, 100, 0xFFFFFF, "とある建造物が存在する。");
+	DrawFormatString(100, 150, 0xFFFFFF, "ダンジョンと呼称されるこの建造物は");
+	DrawFormatString(100, 200, 0xFFFFFF, "いつどのような目的で建造されたのか誰も知らない。");
+	DrawFormatString(100, 250, 0xFFFFFF, "その秘密を知るべく、人々は果敢に挑戦を続けている。");
+	DrawFormatString(100, 300, 0xFFFFFF, "ダンジョンは数十ものフロアで構成されているのだが");
+	DrawFormatString(100, 350, 0xFFFFFF, "何故か至る所に強力なモンスターが存在しており");
+	DrawFormatString(100, 400, 0xFFFFFF, "最終フロアまで到達できた者はいないと言われている。");
+	DrawFormatString(100, 450, 0xFFFFFF, "主人公は、そんな未知を探求している者だ。");
+
+
+
+
+
 	ScreenFlip();
 }
