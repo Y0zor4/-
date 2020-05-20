5cb18dd92c7b754ceb021d77450ad1c6ad18b000
Y0zor4 <kirakirahikaru1333@icloud.com>
2020-05-20 14:56:18 +0900

    ゲームに使用する背景追加 ソースコードの追加

--- TetodannProject/story.cpp
+++ TetodannProject/story.cpp
@@ -35,17 +35,16 @@ void StoryDraw(void)
 	SetFontSize(50);
 	DrawFormatString(0, 0, 0xFFFFFF, "StoryScene");
 	//DrawBox(100, 100, 700, 500, 0x00FF00, true);
-	DrawFormatString(100, 100, 0xFFFFFF, "とある建造物が存在する。");
-	DrawFormatString(100, 150, 0xFFFFFF, "ダンジョンと呼称されるこの建造物は");
-	DrawFormatString(100, 200, 0xFFFFFF, "いつどのような目的で建造されたのか誰も知らない。");
-	DrawFormatString(100, 250, 0xFFFFFF, "その秘密を知るべく、人々は果敢に挑戦を続けている。");
-	DrawFormatString(100, 300, 0xFFFFFF, "ダンジョンは数十ものフロアで構成されているのだが");
-	DrawFormatString(100, 350, 0xFFFFFF, "何故か至る所に強力なモンスターが存在しており");
-	DrawFormatString(100, 400, 0xFFFFFF, "最終フロアまで到達できた者はいないと言われている。");
-	DrawFormatString(100, 450, 0xFFFFFF, "主人公は、そんな未知を探求している者だ。");
-
-
-
+	DrawFormatString(450, 100, 0xFFFFFF, "とある建造物が存在する。");
+	DrawFormatString(300, 200, 0xFFFFFF, "ダンジョンと呼称されるこの建造物は");
+	DrawFormatString(150, 250, 0xFFFFFF, "いつどのような目的で建造されたのか誰も知らない。");
+	DrawFormatString(150, 300, 0xFFFFFF, "その秘密を知るべく、人々は果敢に挑戦を続けている。");
+	DrawFormatString(150, 350, 0xFFFFFF, "ダンジョンは数十ものフロアで構成されているのだが");
+	DrawFormatString(200, 400, 0xFFFFFF, "何故か至る所に強力なモンスターが存在しており");
+	DrawFormatString(150, 450, 0xFFFFFF, "最終フロアまで到達できた者はいないと言われている。");
+	DrawFormatString(250, 500, 0xFFFFFF, "主人公は、そんな未知を探求している者だ。");
+	DrawFormatString(200, 550, 0xFFFFFF, "パズルをすることで何故か攻撃ができる主人公は");
+	DrawFormatString(200, 600, 0xFFFFFF, "これを利用してダンジョンを進んでいくのだった…");
 
 
 	ScreenFlip();
